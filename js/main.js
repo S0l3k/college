8989324b030a71919a4802d34893b290af41371a
S0l3k <turtleteamwcg@gmail.com>
2023-08-16 16:40:11 +0300

    add: block courses and settings for slider

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ const header_swiper = new Swiper('.swiper', {
   },
   },
 );
+
+const courses_swiper = new Swiper('.swiper', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  },
+);
