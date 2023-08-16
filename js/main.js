f551595462df60b65b2d1469f225e084291dd793
S0l3k <turtleteamwcg@gmail.com>
2023-08-16 17:00:32 +0300

    add: block facilities and slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const header_swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
@@ -13,6 +17,28 @@ const header_swiper = new Swiper('.swiper', {
 );
 
 const courses_swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  },
+);
+
+const facilities_swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
